5efb971f8c931ebf04c66096e0078aedb42efdbb
wiler121 <36072171+wiler121@users.noreply.github.com>
2018-11-04 17:58:01 +0100

    Refactoring to common crud intefaces. Closes #11

--- pet-clinic-data/src/main/java/mf/spring/petclinic/services/PetSerive.java
+++ pet-clinic-data/src/main/java/mf/spring/petclinic/services/PetSerive.java
@@ -2,13 +2,8 @@ package mf.spring.petclinic.services;
 
 import mf.spring.petclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetSerive {
 
-    Pet findById(Long id);
+public interface PetSerive extends CrudService<Pet, Long>{
 
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/mf/spring/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mf/spring/petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package mf.spring.petclinic.services;
 
 import mf.spring.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 
 }
