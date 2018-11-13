e9e46aa4beb56b64914d508254aefbfcf8106fea
wiler121 <36072171+wiler121@users.noreply.github.com>
2018-11-13 22:25:20 +0100

    Enhance Owners, info and pets. Closes #31

--- pet-clinic-web/src/main/java/mf/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mf/spring/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package mf.spring.petclinic.bootstrap;
 
 import mf.spring.petclinic.model.Owner;
+import mf.spring.petclinic.model.Pet;
 import mf.spring.petclinic.model.PetType;
 import mf.spring.petclinic.model.Vet;
 import mf.spring.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import mf.spring.petclinic.services.PetTypeService;
 import mf.spring.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -27,22 +30,42 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         PetType dog = new PetType();
-        dog.setName("Doggy");
+        dog.setName("Dog");
         PetType saveDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
-        cat.setName("Catty");
+        cat.setName("Cat");
         PetType saveCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFristName("Michal");
         owner1.setLastName("Flisz");
+        owner1.setAddress("Nistepowo Wloscianskie 1");
+        owner1.setCity("Pokrzywnica");
+        owner1.setTelephone("111222333");
+
+        Pet michalPet = new Pet();
+        michalPet.setPetType(saveDogPetType);
+        michalPet.setOwner(owner1);
+        michalPet.setBirthDate(LocalDate.now());
+        michalPet.setName("WONSZ");
+        owner1.getPets().add(michalPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFristName("Adi");
         owner2.setLastName("Lama");
+        owner2.setAddress("13 pulku piechoty");
+        owner2.setCity("Warszawa");
+        owner2.setTelephone("222333444");
+
+        Pet adiPet = new Pet();
+        adiPet.setName("Catty");
+        adiPet.setOwner(owner2);
+        adiPet.setBirthDate(LocalDate.now());
+        adiPet.setPetType(saveCatPetType);
+        owner2.getPets().add(adiPet);
 
         ownerService.save(owner2);
 
